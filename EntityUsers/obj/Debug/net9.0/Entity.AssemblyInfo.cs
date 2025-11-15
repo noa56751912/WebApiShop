0f74dc9f63c9c312b7883abc665d4257199ac0bf
noa56751912 <n0548545838>
2025-11-15 22:26:23 +0200

    final commit

--- EntityUsers/obj/Debug/net9.0/Entity.AssemblyInfo.cs
+++ EntityUsers/obj/Debug/net9.0/Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ccbcfe0f22194f03ffbc0a225f81c5419a75d6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
